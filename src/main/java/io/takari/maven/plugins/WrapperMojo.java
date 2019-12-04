6b91f5b28ec7de93f2e03b5d8f7d197d4e86a047
Manfred Moser <manfred@simpligility.com>
2019-12-04 12:35:39 -0800

    Update to Maven 3.6.3 as default

--- src/main/java/io/takari/maven/plugins/WrapperMojo.java
+++ src/main/java/io/takari/maven/plugins/WrapperMojo.java
@@ -36,7 +36,7 @@ import io.tesla.proviso.archive.UnArchiver;
 @Mojo(name = "wrapper", requiresProject = false, aggregator = true)
 public class WrapperMojo extends AbstractMojo {
   private static final String DEFAULT_DOWNLOAD_BASE_URL="https://repo.maven.apache.org/maven2";
-  private static final String DEFAULT_MAVEN_VER = "3.6.1";
+  private static final String DEFAULT_MAVEN_VER = "3.6.3";
 
   @Parameter(defaultValue = "${session}", readonly = true)
   private MavenSession session;
